44d73e381230adc336d9505c85bac85f038e2a90
Sandhiya_dev <sandhi871@gmail.com>
2026-04-07 20:38:27 +0530

    Delete day_2 lists.py

--- day_2 lists.py
+++
@@ -1,9 +0,0 @@
-#Find Largest Number
-numbers = [12,34,72,44,92,56]
-largest = max(numbers)
-print("Largest number : ", largest)
-
-#Sum of list
-num=[1,2,3,4,5,6]
-total=sum(num)
-print("The sum of numbers : ", total)
