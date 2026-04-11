37c5d31b18a3bfacc74ed237e0f75425fb4799c0
Sandhiya_dev <sandhi871@gmail.com>
2026-04-11 12:24:59 +0530

    File handling automation script practice file

---
+++ file_word_counter.py
@@ -0,0 +1,12 @@
+f=open('python_practice/MyData.txt','r')
+f1=f.read()
+words=f1.split()
+lines=f1.splitlines()
+print(f"No of Words: {len(words)}")
+print(f"No of lines: {len(lines)}")
+ 
+
+
+    
+
+   
