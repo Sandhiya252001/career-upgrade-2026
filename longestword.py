8f5df93412e7401611657e5e09e08a81a2407683
Sandhiya_dev <sandhi871@gmail.com>
2026-04-14 17:51:38 +0530

    Delete longestword.py

--- longestword.py
+++
@@ -1,7 +0,0 @@
-string="My name is sandhiya subramani"
-splstr=string.split(" ")
-longest=""
-for splstrs in splstr:
-    if len(splstrs)>len(longest):
-        longest=splstrs
-print(f"Longest Word is : {longest}")
