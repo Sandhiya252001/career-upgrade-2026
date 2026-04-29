544486837b94ccf36ac08b4b5b6b53f5f3355041
Sandhiya252001 <sandhi871@gmail.com>
2026-04-29 14:40:58 +0530

    Python zip

---
+++ python_practice/zip.py
@@ -0,0 +1,9 @@
+names=('Arun','Davis','Leon','Narmen')
+Comps=('Dell','Apple','Samsung','Dell')
+Zipped=zip(names,Comps)
+for (names,Comps) in Zipped:
+    print(names,Comps) 
+prod=('pen','pencil','pelicon','eraser')
+quantiy=(1,2,3,4)
+ZippedProd=list(zip(prod,quantiy))
+print(ZippedProd)
