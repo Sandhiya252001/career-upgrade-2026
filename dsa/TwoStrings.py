588ae4d76b00fd7840a0598195a751795de961ed
Sandhiya252001 <sandhi871@gmail.com>
2026-05-19 10:34:33 +0530

    DSA

---
+++ dsa/TwoStrings.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def twoStrings(s1, s2):
+    r=0
+    for i in s1:
+        counter=0
+        for j in s2:
+            if j==i:
+                counter+=1
+        r=counter
+    if r>=1:
+        return "YES"
+    else:
+        return "NO"
+
+                
+        
+  
+    
+        
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+    #     fptr.write(result + '\n')
+
+    # fptr.close()
