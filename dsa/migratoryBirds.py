2a5e0121c837aca136dce27f9582cd9941d3d3ed
Sandhiya252001 <sandhi871@gmail.com>
2026-05-31 16:46:38 +0530

    DSA

---
+++ dsa/migratoryBirds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    freq=Counter(arr)
+    max_count=max(freq.values())
+    result=min(key for key,value in freq.items() if value==max_count)
+    return result
+        
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
