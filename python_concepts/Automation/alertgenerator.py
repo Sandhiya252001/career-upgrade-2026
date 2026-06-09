62603ef03ba42e6b42664cf10ad5bf813d100269
Sandhiya252001 <sandhi871@gmail.com>
2026-06-09 10:30:08 +0530

    Automation

---
+++ python_concepts/Automation/alertgenerator.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+Err=0
+Info=0
+Warn=0
+with open(r"C:\Users\sandhiya.c.subramani\Desktop\Python\Day 01\career_upgrade_2026\python_concepts\Automation\sample.log") as f:
+    line=f.readlines()
+    for i in line:
+        if "ERROR" in i:
+            Err+=1
+        if "WARNING" in i:
+            Warn+=1
+        if "INFO" in i:
+            Info+=1
+Total=len(line)
+ErrPer=(Err/Total)*100
+date=datetime.now()
+print(date.strftime("%Y-%m-%d %H:%M:%S"))
+if ErrPer<60:
+    SystemHealth="Healthy"
+else:
+    SystemHealth="Warning"
+report = (
+    f"Total Logs: {Total}\n"
+    f"INFO Count: {Info}\n"
+    f"ERROR Count: {Err}\n"
+    f"WARNING Count: {Warn}\n"
+    f"Error Rate: {ErrPer:.2f}%\n"
+    f"Status: {SystemHealth}")
+print(report)
